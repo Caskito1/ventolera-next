f025100c69847dd35a69380d1870e66a2c8e1ae5
Agustín Billar <abillar@b2blatam.com>
2026-01-28 18:13:25 -0300

    Dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -24,21 +24,30 @@ export default function DashboardPage() {
 
   return (
     <div className="flex p-6 gap-6">
+          {/* Fondo */}
+      <div className="absolute inset-0 -z-10">
+        <div
+          className="w-full h-full bg-fixed bg-center bg-cover"
+          style={{ backgroundImage: "url('/media/bg-modales/coope-modal.webp')" }}
+        />
+        <div className="absolute inset-0 bg-gray-900/80" />
+        <div className="absolute inset-0 bg-[url('/media/patterns/noise.webp')] opacity-30 mix-blend-overlay pointer-events-none" />
+      </div>
       {/* Columna izquierda - Tabs */}
-      <div className="w-64 bg-gray-100 p-4 rounded-lg shadow flex flex-col">
+      <div className="w-64 backdrop-blur-md bg-white/20 text-white p-4 rounded-lg shadow flex flex-col h-[95vh]">
         <h2 className="text-xl font-bold mb-2">{userData.nombre}</h2>
-        <p className="text-sm text-gray-600 mb-4">{userData.rol}</p>
+        <p className="text-sm text-white-600 mb-4 ">{userData.rol}</p>
 
         <div className="flex flex-col gap-2 mb-4">
           <button
-            className={`py-2 px-3 rounded ${selectedTab === "partituras" ? "bg-blue-500 text-white" : "bg-white text-gray-700"}`}
+            className={`py-2 px-3 rounded ${selectedTab === "partituras" ? "bg-orange-500/80 hover:bg-orange-600/90 text-white font-semibold py-2 rounded-md w-full transition-all duration-200 backdrop-blur-sm shadow-md hover:shadow-lg" : "bg-white text-gray-700"}`}
             onClick={() => setSelectedTab("partituras")}
           >
             Partituras
           </button>
           {userData.rol === "admin" && (
             <button
-              className={`py-2 px-3 rounded ${selectedTab === "administracion" ? "bg-blue-500 text-white" : "bg-white text-gray-700"}`}
+              className={`py-2 px-3 rounded ${selectedTab === "administracion" ? "bg-orange-500/80 hover:bg-orange-600/90 text-white font-semibold py-2 rounded-md w-full transition-all duration-200 backdrop-blur-sm shadow-md hover:shadow-lg" : "bg-white text-gray-700"}`}
               onClick={() => setSelectedTab("administracion")}
             >
               Administración
--- app/log-in/page.jsx
+++ app/log-in/page.jsx
@@ -23,10 +23,19 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+    <div className="flex items-center justify-center min-h-screen ">
+       {/* Fondo */}
+      <div className="absolute inset-0 -z-10">
+        <div
+          className="w-full h-full bg-fixed bg-center bg-cover"
+          style={{ backgroundImage: "url('/media/bg-modales/coope-modal.webp')" }}
+        />
+        <div className="absolute inset-0 bg-gray-900/80" />
+        <div className="absolute inset-0 bg-[url('/media/patterns/noise.webp')] opacity-30 mix-blend-overlay pointer-events-none" />
+      </div>
       <form
         onSubmit={handleLogin}
-        className="bg-white shadow-lg rounded-xl p-6 w-80"
+        className="backdrop-blur-md bg-white/10 border border-white/20 shadow-xl rounded-2xl p-8 w-80 text-center text-white"
       >
         <h1 className="text-xl font-bold mb-4 text-center">Iniciar sesión</h1>
 
@@ -52,7 +61,7 @@ export default function LoginPage() {
 
         <button
           type="submit"
-          className="bg-blue-600 text-white w-full py-2 rounded hover:bg-blue-700"
+           className="bg-orange-500/80 hover:bg-orange-600/90 text-white font-semibold py-2 rounded-md w-full transition-all duration-200 backdrop-blur-sm shadow-md hover:shadow-lg"
         >
           Entrar
         </button>
